7efdc74ecb94dce4104718e167622214cbebb915
Jonas Richter <SeaGrass@users.noreply.github.com>
2015-08-31 18:04:36 +0200

    Erster Upload

---
+++ Stochastik-App/app/src/main/java/com/alphareborn/stochastik_app/MainActivity.java
@@ -0,0 +1,23 @@
+package com.alphareborn.stochastik_app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+
+public class MainActivity extends FragmentActivity {
+	
+	public static final String ARG_PAGE = "page";
+	ViewPager viewpager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		viewpager = (ViewPager) findViewById(R.id.pager);
+		PagerAdapter padapter = new InfinitePagerAdapter(getSupportFragmentManager());
+		viewpager.setAdapter(padapter);
+
+		
+	}
+}
---
+++ Stochastik-App/app/src/main/java/com/alphareborn/stochastik_app/PagerAdapter.java
@@ -0,0 +1,44 @@
+package com.alphareborn.stochastik_app;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+	private static final int NUM_PAGES = 5;
+	
+	public PagerAdapter(FragmentManager fm) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+	
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		switch (arg0) {
+		case 0:
+			return new FragmentOne();
+		case 1:
+			return new FragmentTwo();
+		case 2:
+			return new FragmentThree();
+		case 3:
+			return new FragmentFour();
+		case 4:
+			return new FragmenDummyTwo();
+
+		default:
+			break;
+		}
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return NUM_PAGES;
+	}
+
+}
